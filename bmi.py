40f362bf3f0e2e4170ebbac823bef1064499d033
muhammadamerhassan <amerfabrics@gmail.com>
2024-08-31 16:13:15 +0000

    Convert into Functions

--- bmi.py
+++ bmi.py
@@ -1,4 +1,7 @@
+def Calculate_BMI(weight:float, height:float) -> float:
+    return weight/(height ** 2)
+
 weight:float = float(input("Enter weight in Kilograms: "))
 height:float = float(input("Enter height in meters: "))
-BMI:float = weight/(height ** 2)
+BMI:float = Calculate_BMI(weight, height)
 print("The BMI (Body Mass Index) is ", round(BMI,2))
