14229ebe2767223b431d3328e1cc51b1bd41b27f
muhammadamerhassan <amerfabrics@gmail.com>
2024-08-31 16:32:19 +0000

    convert to functions

--- Cylinder_Volume.py
+++ Cylinder_Volume.py
@@ -1,6 +1,10 @@
 import math # importing math library for the exact value of PI
 
+def Cylinder(radius:float, height:float) -> float:
+    return math.pi * (radius ** 2) * height
+
+
 radius:float = float(input("Enter the radius of a cylinder: "))
 height:float = float(input("Enter the height of a cylinder: "))
-volume:float = math.pi * (radius ** 2) * height
+volume:float = Cylinder(radius, height)
 print(f"The volume of Cylinder is {volume} m³")
