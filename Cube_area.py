5336104bda77bc9cc89fa4ff3b2694c486df104d
muhammadamerhassan <amerfabrics@gmail.com>
2024-08-31 16:29:33 +0000

    convert to functions

--- Cube_area.py
+++ Cube_area.py
@@ -1,3 +1,7 @@
+def Cube_area(length:float) -> float:
+    return 6 * (length ** 2)
+
+
 length:float = float(input("Enter length of one side of a cube: "))
-area:float = 6 * (length ** 2)
-print("The area of a cube is: ", area)
+area:float = Cube_area(length)
+print("The area of a cube is: ", area) 
