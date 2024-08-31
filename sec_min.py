bb7cdf7092d67ffd90bb99812df24dec0bd8644b
muhammadamerhassan <amerfabrics@gmail.com>
2024-08-31 16:43:39 +0000

    convert to functions

--- sec_min.py
+++ sec_min.py
@@ -26,4 +26,4 @@ if __name__ == "__main__":
             case "3":
                 break
             case _:
-                print("invalid choice")            
+                print("invalid choice")                        
