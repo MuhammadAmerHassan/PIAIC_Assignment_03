5c57eac5ccaccdd28b3032e7bc6de99b7877dc97
muhammadamerhassan <amerfabrics@gmail.com>
2024-08-31 16:26:09 +0000

    convert to functions

--- Circle_area.py
+++ Circle_area.py
@@ -1,4 +1,8 @@
 import math # Import Math library for exact value of PI
+
+def Calculate_area(radius:float)-> float:
+    return math.pi * (radius ** 2)
+
 radius:float = float(input("Enter radius of a circle: "))
-area = math.pi * (radius ** 2)
+area = Calculate_area(radius)
 print("Area of circle is: ", area)
