fecb507210e4ff4298b739037504512fad2a4162
muhammadamerhassan <amerfabrics@gmail.com>
2024-08-31 16:36:23 +0000

    convert to functions

--- Percentage.py
+++ Percentage.py
@@ -1,4 +1,7 @@
+def Percentage(Total_Value:float, Actual_Value:float) -> float:
+    return (Actual_Value/Total_Value) * 100
+
 Total_Value:float = float(input("Enter Total Value: "))
 Actual_Value:float = float(input(f"Enter actual value to calculate percenatge from {Total_Value}: "))
-percentage:float = (Actual_Value/Total_Value) * 100
+percentage:float = Percentage(Total_Value, Actual_Value)
 print(f"The answer or result is {round(percentage,2)}%")
