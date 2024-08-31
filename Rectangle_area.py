cc8be8cfad6449f1495770b2bd3625bc9a55c224
muhammadamerhassan <amerfabrics@gmail.com>
2024-08-31 16:35:11 +0000

    convert to functions

--- Rectangle_area.py
+++ Rectangle_area.py
@@ -1,4 +1,7 @@
+def Rectangle_area(length:float, width:float) -> float:
+    return length * width
+
 length:float = float(input("Enter length of a rectangle: "))
 width:float = float(input("Enter width of a rectangle: "))
-area:float = length * width
+area:float = Rectangle_area(length, width)
 print("Area of Rectangle is: ", area)
